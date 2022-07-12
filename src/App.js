6942adc81a91358a8d7934d612b079e142ad4374
김재만 <daga4242@gmail.com>
2022-07-12 11:22:43 +0900

    feat: LineChart-PieChart

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import "./App.css";
 import BarChart from "./components/BarChart";
 import { useState } from "react";
 import { UserData } from "./Data";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 function App() {
   const [userData, setUserData] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        label: "Users Lost",
+        data: UserData.map((data) => data.userLost),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#F3BA2F",
+          "#2A71D0",
+          "#000",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
       },
     ],
   });
@@ -19,6 +31,12 @@ function App() {
     <div className="App">
       <div style={{ width: 700 }}>
         <BarChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
       </div>
     </div>
   );
